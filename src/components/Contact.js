516a4943f0a4c5b8bb92b51225d7fd1e8a02b4c6
AlanNahuelAragon <alan.aragon.exe@gmail.com>
2023-04-06 00:32:07 -0300

    Contact Form and emailJS added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+  const sendEmail = (event) => {
+    event.preventDefault();
+    emailjs.sendForm('service_ov3awyk', 'template_a5jeuzu', event.target, 'ly1ZUO_-kRkRHnOVY')
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+  }
   return (
     <div className='Contact'>
       <div className='MiniContainer'>
@@ -8,14 +15,32 @@ function Contact() {
           {"<CONTACT>"}
         </div>
         <div className='ContactContainer'>
-          Contact
-          <button class="button-spaceship-pushable" type='button'>
-            <span class="button-spaceship-shadow"></span>
-            <span class="button-spaceship-edge"></span>
-            <span class="button-spaceship-front text">
-              Enviar!
-            </span>
-          </button>
+          <div className='div-form'>
+            <h1 className='title-form'>Contact</h1>
+            <form className='form-mail' onSubmit={sendEmail}>
+              <label>Name</label>
+              <input type='text' name='user_name'></input>
+              <hr />
+
+              <label>Email</label>
+              <input type='email' name='user_email'></input>
+              <hr />
+
+              <label>Message</label>
+              <textarea name='user_message' id="" cols={30} rows={10}></textarea>
+              <hr />
+              <button className='button-spaceship-pushable '>
+                <span class="button-spaceship-shadow"></span>
+                <span class="button-spaceship-edge"></span>
+                <span class="button-spaceship-front text">
+                  Send Message!
+                </span>
+              </button>
+
+            </form>
+
+          </div>
+
         </div>
         <div className='SectionTitle'>
           {"</CONTACT>"}
